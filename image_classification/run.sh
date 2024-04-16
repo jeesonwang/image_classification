83e9c5846bac458a9757cd2d6c35d3890c126755
wangjiansheng <wangjeeson@gmail.com>
2024-04-16 10:42:47 +0800

    modify

--- image_classification/run.sh
+++ image_classification/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PYTHONPATH=$PYTHONPATH:/data5/shnu/wjs/image_classification
 export CUDA_VISIBLE_DEVICES='3'
 
 python3 main.py --arch resnet --depth 56 --save save/cifar100-resnet-56 --data cifar100 --data_aug
